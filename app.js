27229ff12f3c81b001ab6de95bbc66dd04aca5e7
Yesha Joshi <joshi.ye@northeastern.edu>
2024-11-12 20:04:52 -0500

    nested elements

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-const heading = React.createElement(
-    'h1', 
-    {id : 'heading'}, 
-    'Hello World from React'); //3 arguments -> tag, object(place where we give attributes to tags), content
+{/* <div id=""parent>
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+    <div id="child">
+        <h1></h1>
+        <h2></h2>
+    </div>
+</div> */}
 
-console.log(heading); //react element object, also has props(children)
+
+//parent is just an object which is a react element which becomes HTMl which the browser can understand
+const parent = React.createElement(
+    'div', {id : 'parent'},
+    /*children of div id parent*/ [React.createElement( //array of children of div tags which are siblings
+                                            'div', {id : 'child1'}, 
+                                            [React.createElement('h1', {}, "I am an h1 tag"), React.createElement('h2', {}, "I am an h2 tag")] //array of children to create siblings
+                                        ), React.createElement(
+                                            'div', {id : 'child2'}, 
+                                            [React.createElement('h1', {}, "I am an h1 tag"), React.createElement('h2', {}, "I am an h2 tag")]
+                                        )]
+                                    ); //nested elements
+
+
+// const heading = React.createElement(
+//     'h1', 
+//     {id : 'heading'}, 
+//     'Hello World from React'); //3 arguments -> tag, object(place where we give attributes to tags), content
+
+console.log(parent); //react element object, also has props(children)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(parent);
